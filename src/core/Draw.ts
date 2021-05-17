c6d3b1069c4c7cd75c821dd0c0a6269baa931a8e
Sabbir Ahmed <sabbiirr@gmail.com>
2021-05-18 00:44:04 +0600

    make render method private

--- src/core/Draw.ts
+++ src/core/Draw.ts
@@ -1,6 +1,17 @@
 import { DrawFile } from "./DrawFile";
 import type { Shape } from "../objects/Shape";
 
+/**
+ * Code blocks are great for examples
+ *
+ * ```typescript
+ * import { Draw } from '@thesabbir/drawjs';
+ *
+ * const rootElement = document.queryeSlector('root');
+ * const draw = new Draw(rootElement);
+ *
+ * ```
+ */
 export class Draw {
   private readonly _rootElm: HTMLDivElement;
   private readonly _files: Map<DrawFile, DrawFile>;
@@ -42,7 +53,7 @@ export class Draw {
     }
   }
 
-  _renderDom(content = this.activeFile.toSVG()) {
+  private _renderDom(content = this.activeFile.toSVG()) {
     if (!this._rootElm) {
       return new Error("Root element not found");
     }
